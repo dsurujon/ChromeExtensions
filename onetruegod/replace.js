ab23f5f84e4dd6b2f7351e942cadad20d8f0221d
Defne <defnesurujon@gmail.com>
2015-03-15 11:03:27 -0400

    Update replace.js

--- onetruegod/replace.js
+++ onetruegod/replace.js
@@ -1,2 +1,2 @@
-#Because He is the One  True God
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Nicolas Cage","g"), "One True God");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Cage","g"), "His Holiness");
